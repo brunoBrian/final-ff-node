5405bb217d366c4defda6182e7d6b608880fbe69
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-05-26 23:10:18 -0300

    adicionando resource de comentário

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommentModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
