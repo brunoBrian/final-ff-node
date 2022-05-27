1d28ba3b3dc2974a961913ce03c3e9a232ffa5ed
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-05-27 11:20:51 -0300

    criando array de comentários e adicionando no metodo get, para retornar todos os comentarios

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CommentEntity } from './entities/comment.entity';
 
 @Injectable()
 export class CommentService {
+  private comment: CommentEntity[] = [
+    {
+      id: '123',
+      comment: 'Some comment',
+      user_id: '1',
+    },
+  ];
+
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
 
   findAll() {
-    return `This action returns all comment`;
+    return this.comment;
   }
 
   findOne(id: number) {
--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,5 @@
-export class Comment {}
+export class CommentEntity {
+  id: string;
+  user_id: string;
+  comment: string;
+}
