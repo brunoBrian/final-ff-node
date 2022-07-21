c8f8d3c98bd708c93ee8bde7ee03e7859d07fd84
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-07-21 17:21:13 -0300

    descrevendo e atribuindo valor padrão na doc do swagger

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 import { CommentEntity } from '../entities/comment.entity';
 
 export class CreateCommentDto extends CommentEntity {
+  @ApiProperty({
+    description: 'ID de um usuario existente',
+    example: '12345',
+  })
   @IsString()
   user_id: string;
 
+  @ApiProperty({
+    description: 'Comentário',
+    example: 'Algum comentário',
+  })
   @IsString()
   comment: string;
 }
--- src/comment/dto/update-comment.dto.ts
+++ src/comment/dto/update-comment.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 import { CreateCommentDto } from './create-comment.dto';
 
 export class UpdateCommentDto extends PartialType(CreateCommentDto) {
+  @ApiProperty({
+    description: 'Comentário',
+    example: 'Algum comentário',
+  })
   @IsString()
   comment: string;
 }
