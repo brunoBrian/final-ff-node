5400e57e1d24e2896450e002a6a041b35dbc5c15
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-10-04 18:29:50 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   // Interceptor
   app.useGlobalInterceptors(new NotFoundInterceptor());
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
